2d1e51d177d3dfaf460ffb459dd28117fda04c4a
Viktor537 <vikibojilov537@gmail.com>
2025-05-30 13:31:20 +0300

    feat: Setup basic routing and navigation structure

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Welcome to our Recipe Application!</p>
+    </div>
+  );
+};
+export default HomePage;
---
+++ src/pages/RecipesPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RecipesPage = () => {
+  return (
+    <div>
+      <h1>Recipes Page</h1>
+    </div>
+  );
+};
+export default RecipesPage;
